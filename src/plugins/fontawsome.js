b51434b6c4c6edd9050fcedcd5c7c500d7ff01de
Henry <he790803@gmail.com>
2023-05-04 03:51:35 +0800

    Updates

--- src/plugins/fontawsome.js
+++
@@ -1,5 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
-library.add(faShoppingCart, faBars, faXmark);
